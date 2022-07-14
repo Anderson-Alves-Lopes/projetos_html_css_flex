2897547496b3d8819d8057cd9dc54d3c24aaface
Anderson Alves <nenoalveslopes@hotmail.com>
2022-07-13 23:48:37 -0300

    "Alterações no menu mobile deixando mais suave"

--- projeto_awax/js/script.js
+++ projeto_awax/js/script.js
@@ -1,9 +1,11 @@
 
 function openMenu(){
     let openNav = document.querySelector('nav');
-   if(openNav.style.display == 'none'){
-     openNav.style.display = 'flex';
-   }else {   
-    openNav.style.display = 'none';
+   if(openNav.style.opacity === '1'){
+     openNav.style.opacity = '0';
+     openNav.style.width = '0';
+   }else if(openNav.style.opacity = '0'){  
+    openNav.style.opacity = '1';
+    openNav.style.width = '50vw';
    }
 }
